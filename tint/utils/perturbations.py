f3d8b0039d9c6be6db216bb158ded04ef14ebfb9
Paulius Skaisgiris <paulius.skaisgiris@gmail.com>
2024-02-16 00:50:02 +0100

    feat: add necessary files for extensions

---
+++ tint/utils/perturbations.py
@@ -0,0 +1,124 @@
+import torch as th
+import torch.nn as nn
+from tint.attr.models import ExtremalMaskNet, MaskNet
+from tint.models import Net
+from torch.nn.functional import normalize
+
+
+def compute_perturbations(
+    data: th.Tensor,
+    mask_net: Net,
+    perturb_net: nn.Module = None,
+    batch_idx: int or None = 0,
+) -> (th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor):
+    """Use the trained mask network to compute perturbations and their intermediate results.
+
+    This function only works for explainer methods that train a mask network. Such methods include DynaMask and ExtremalMask.
+
+    :param data: all data to be investigated. A subset of it will be investigated depending on the batch_idx and batch_size of the mask_net
+    :param mask_net: the trained mask network
+    :param perturb_net: the trained perturbation network. You can usually access it via mask_net.net.model. Defaults to None in which case the baseline will be zeroes
+    :param batch_idx: the batch number to investigate, batch size will be inferred from mask_net. If set to None, all supplied data will be considered. Defaults to 0
+    :return: a 5-tuple:
+    - tensor of the batch of data that was used for calculation
+    - tensor of perturbations predicted by perturb_net (or defaulted to)
+    - tensor of the predicted mask by mask_net
+    - tensor of inputs with important features masked, i.e. from the paper: data * mask + (1 - mask) * NN(data)
+    - tensor of inputs with unimportant features masked, i.e.: data * (1 - mask) + NN(data) * data
+    """
+    if isinstance(mask_net, MaskNet):
+        return _compute_perturbations_dynamask(data, mask_net, batch_idx)
+    elif isinstance(mask_net, ExtremalMaskNet):
+        return _compute_perturbations_extremal(data, mask_net, perturb_net, batch_idx)
+    else:
+        raise NotImplementedError(
+            "Perturbations can only be computed for ExtremalMask and DynaMask methods but mask_net is neither"
+        )
+
+
+def _compute_perturbations_extremal(
+    data: th.Tensor,
+    mask_net: ExtremalMaskNet,
+    perturb_net: nn.Module = None,
+    batch_idx: int or None = 0,
+) -> (th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor):
+    if batch_idx is None:
+        from_b = 0
+        to_b = len(data)
+    else:
+        batch_size = mask_net.net.batch_size
+        from_b = batch_size * batch_idx
+        to_b = batch_size * (batch_idx + 1)
+
+    batch = data[from_b:to_b]
+
+    # If there is a model, NN(x), that predicts perturbations, use it
+    # Otherwise, default to zeroes
+    baseline = perturb_net(batch) if perturb_net is not None else th.zeros_like(batch)
+
+    # Retrieve the learned mask for the batch in question
+    mask = mask_net.net.mask
+    mask = mask[from_b:to_b]
+    mask = mask.clamp(0, 1)
+
+    # Mask data according to samples
+    # We eventually cut samples up to x time dimension
+    # x1 represents inputs with important features masked.
+    # x2 represents inputs with unimportant features masked.
+    mask = mask[:, : batch.shape[1], ...]
+    x1 = batch * mask + baseline * (1.0 - mask)
+    x2 = batch * (1.0 - mask) + baseline * mask
+
+    return batch, baseline.detach(), mask.detach(), x1.detach(), x2.detach()
+
+
+def _compute_perturbations_dynamask(
+    data: th.Tensor, mask_net: MaskNet, batch_idx: int
+) -> (th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor):
+    if mask_net.net.perturbation == "fade_moving_average":
+        return _fade_moving_average(x=data, mask_net=mask_net, batch_idx=batch_idx)
+    else:
+        raise NotImplementedError(
+            "Only fade_moving_average is implemented for fixed-function perturbation"
+        )
+
+
+def _fade_moving_average(
+    x: th.Tensor, mask_net: MaskNet, batch_idx: int
+) -> (th.Tensor, th.Tensor, th.Tensor, th.Tensor, th.Tensor):
+    """Computes perturbation components using fade moving average.
+
+    Adapted from MaskNet class.
+    """
+    net = mask_net.net
+    mask = 1.0 - net.mask if net.deletion_mode else net.mask
+
+    # Do not use net.keep_ratio here because we only use the first
+    # ratio
+    from_b = net.batch_size * batch_idx
+    to_b = net.batch_size * (batch_idx + 1)
+    mask = mask[from_b:to_b]
+    x = x[from_b:to_b]
+
+    moving_average = th.mean(x, 1).unsqueeze(1)
+    x1 = mask * x + (1 - mask) * moving_average
+    x2 = x * (1 - mask) + moving_average * mask
+    return x.detach(), moving_average.detach(), mask.detach(), x1.detach(), x2.detach()
+
+
+def compute_alternative(
+    batch: th.Tensor,
+    mask: th.Tensor,
+    perturbation: th.Tensor,
+    normalize_along_dim: int or tuple = (1, 2),
+    p: int = 2,
+) -> th.Tensor:
+    """Compute alternative saliency metric.
+
+    1 - (1 - m) * |NN(x) - x|
+    Intuition: the higher this value, the more perturbed the data is.
+    """
+    normalized_alt = th.abs(perturbation - batch)
+    normalized_alt = normalize(normalized_alt, dim=normalize_along_dim, p=p)
+    alternative = 1 - (1 - mask) * normalized_alt
+    return alternative
---
+++ tint/utils/plotting.py
@@ -0,0 +1,335 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import torch as th
+from matplotlib.lines import Line2D
+from scipy.stats import t
+
+
+# Adapted from https://github.com/fzi-forschungszentrum-informatik/TSInterpret/blob/main/TSInterpret/InterpretabilityModels/Saliency/Saliency_Base.py
+def plot_saliency(
+    ts_data,
+    saliency_list,
+    perturbed=None,
+    feature_names=None,
+    figsize=None,
+    suptitle=None,
+):
+    if feature_names is not None:
+        assert len(feature_names) == len(
+            ts_data
+        ), "The length of feature names and data is not the same"
+
+    plt.style.use("default")
+
+    if figsize is None:
+        figsize = (10, len(ts_data) * 1.2)
+
+    fig, axn = plt.subplots(len(ts_data), 1, sharex=True, sharey=True, figsize=figsize)
+    cbar_ax = fig.add_axes([0.91, 0.3, 0.03, 0.4])
+
+    for i, channel in enumerate(ts_data):
+        axn012 = axn[i].twinx()
+
+        # Plot saliency
+        sns.heatmap(
+            saliency_list[i].reshape(1, -1),
+            fmt="g",
+            cmap="viridis",
+            cbar=i == 0,
+            cbar_ax=None if i else cbar_ax,
+            cbar_kws={"label": "Saliency / Mask intensity"},
+            ax=axn[i],
+            yticklabels=False,
+            vmin=0,
+            vmax=1,
+        )
+
+        xs = range(1, len(channel.reshape(-1)) + 1)
+
+        if perturbed is not None:
+            # Plot learned perturbations
+            axn012.plot(
+                xs,
+                perturbed[i].detach().numpy(),
+                color="red",
+                label="Perturbed data",
+                marker=".",
+            )
+
+        # Plot original data
+        axn012.plot(
+            xs, channel.flatten(), color="#91BBDE", label="Original data", marker="."
+        )
+
+        plt.xlabel("Time", fontweight="bold", fontsize="large")
+        feat_name = feature_names[i] if feature_names is not None else f"Feature {i}"
+        plt.ylabel(feat_name, fontweight="bold", fontsize="medium")
+    plt.legend()
+    fig.tight_layout(rect=[0, 0, 0.9, 1])
+    fig.suptitle(suptitle, y=1.01)
+    plt.show()
+
+
+# Adapted from appendix of https://arxiv.org/abs/2106.05303
+def plot_heatmap(
+    saliencies,
+    figsize=None,
+    title: str = "Mask coefficients over time",
+    subtitles: list or None = None,
+    cbar_title: str = "Saliency / Mask intensity",
+):
+    plt.style.use("default")
+    N = len(saliencies)
+    if figsize is None:
+        figsize = (10, N * 2)
+
+    fig, axn = plt.subplots(int(np.ceil(N / 2)), 2, figsize=figsize)
+    axn = axn.flatten()
+    cbar_ax = fig.add_axes([0.91, 0.3, 0.03, 0.4])
+
+    color_map = sns.diverging_palette(10, 133, as_cmap=True)
+
+    for idx, (method, saliency) in enumerate(saliencies.items()):
+        sns.heatmap(
+            saliency,
+            cmap=color_map,
+            cbar=idx == 0,
+            cbar_ax=None if idx else cbar_ax,
+            cbar_kws={"label": cbar_title},
+            ax=axn[idx],
+            yticklabels=True,
+            linecolor="#d8cbd5",
+            linewidths=0.4,
+            vmin=0,
+            vmax=1,
+        )
+        axn[idx].tick_params(axis="both", which="major", labelsize=7)
+        if subtitles is not None:
+            axn[idx].set_title(subtitles[idx])
+        else:
+            axn[idx].set_title(f"Method: {method}")
+        axn[idx].set_xlabel("Time")
+        axn[idx].set_ylabel("Feature number")
+
+    fig.suptitle(title)
+    fig.tight_layout(rect=[0, 0, 0.9, 1])
+    plt.show()
+
+
+def plot_topk_heatmap(
+    saliencies,
+    subtitles: list or None = None,
+    figsize=None,
+):
+    plt.style.use("default")
+    N = len(saliencies)
+    if figsize is None:
+        figsize = (10, N * 2)
+
+    fig, axn = plt.subplots(int(np.ceil(N / 2)), 2, figsize=figsize)
+    axn = axn.flatten()
+    color_map = sns.diverging_palette(10, 133, as_cmap=True)
+
+    legend_elements = [
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="s",
+            markerfacecolor="#398649",
+            label="Salient",
+            markersize=15,
+        ),
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="s",
+            markerfacecolor="#DA3B46",
+            label="Non-salient",
+            markersize=15,
+        ),
+    ]
+
+    for idx, (method, saliency) in enumerate(saliencies.items()):
+        sns.heatmap(
+            saliency,
+            cmap=color_map,
+            cbar=False,
+            ax=axn[idx],
+            yticklabels=True,
+            linecolor="#d8cbd5",
+            linewidths=0.4,
+            vmin=0,
+            vmax=1,
+        )
+        axn[idx].tick_params(axis="both", which="major", labelsize=7)
+        if subtitles:
+            axn[idx].set_title(method)
+        else:
+            axn[idx].set_title(f"Method: {method}")
+        axn[idx].set_xlabel("Time")
+        axn[idx].set_ylabel("Feature number")
+
+    fig.tight_layout(rect=[0, 0, 0.92, 1])
+    fig.legend(
+        handles=legend_elements,
+        loc="center left",
+        bbox_to_anchor=(0.9, 0.5),
+        bbox_transform=fig.transFigure,
+    )
+    plt.show()
+
+
+def plot_components(
+    orig, perturbed, mask, perturbation, suptitle=None, figsize=(10, 6)
+):
+    plt.style.use("ggplot")
+    plt.rcParams.update({"axes.titlesize": "medium"})
+
+    fig, axs = plt.subplots(5, 1, figsize=figsize)
+
+    orig = orig.flatten().numpy()
+    perturbed = perturbed.detach().numpy()
+    mask = mask.detach().numpy()
+    perturbation = perturbation.detach().numpy()
+
+    xs = range(0, len(orig.reshape(-1)))
+    axs[0].plot(xs, mask, marker=".")
+    axs[0].set_title("Learned mask/saliency: m")
+
+    axs[1].plot(xs, perturbation, marker=".")
+    axs[1].set_title(
+        "Learned perturbation: NN(x)",
+    )
+
+    axs[2].plot(xs, mask * orig, marker=".")
+    axs[2].set_title("m * x")
+
+    axs[3].plot(xs, (1 - mask) * perturbation, marker=".")
+    axs[3].set_title("(1 - m) * NN(x)")
+
+    axs[4].plot(xs, perturbed, label="m * x + (1 - m) * NN(x)", marker=".")
+    axs[4].plot(xs, orig, label="Original data", marker=".")
+    axs[4].set_title("Perturbed vs Original data")
+    axs[4].legend(fontsize=7)
+
+    plt.xlabel("Time", fontweight="bold")
+    plt.suptitle(suptitle)
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_mean_attributions(
+    attr,
+    alpha=0.95,
+    decay: bool = False,
+    title=None,
+    xtick_labels=None,
+    debug=False,
+    figsize=(10, 6),
+    show_legend=True,
+):
+    def compute_means_errors(means_over_time):
+        # Compute means and confidence intervals
+        feature_means = []
+        feature_err = []
+        for feature in means_over_time.T:
+            mean = feature.mean()
+            feature_means.append(mean)
+            conf_interval = t.interval(
+                confidence=alpha,
+                df=len(feature) - 1,
+                loc=np.mean(feature),
+                scale=np.std(feature) / np.sqrt(len(feature)),
+            )
+            # This is needed for the plotting function only, it will always subtract
+            # and add these confidence intervals to the mean
+            conf_interval = (mean - conf_interval[0], conf_interval[1] - mean)
+            feature_err.append(conf_interval)
+        return np.array(feature_means), np.array(feature_err)
+
+    plt.style.use("default")
+    plt.figure(figsize=figsize)
+    if decay:
+        time_horizon = attr.shape[1]  # Assuming time dimension is 1
+        # Compute weight decays showing which time data was most important
+        linear_decay_weights = [
+            1 - ((time_horizon - t) / time_horizon) for t in range(1, time_horizon + 1)
+        ]
+        exp_decay_weights = [
+            np.e ** (-(time_horizon - t) / time_horizon)
+            for t in range(1, time_horizon + 1)
+        ]
+
+    means_over_time = []
+    means_over_time_lin_dec = []
+    means_over_time_exp_dec = []
+    # Compute means per time series (or feature)
+    for patient in attr:
+        if decay:
+            means_over_time_lin_dec.append(
+                np.average(patient, axis=0, weights=linear_decay_weights)
+            )
+            means_over_time_exp_dec.append(
+                np.average(patient, axis=0, weights=exp_decay_weights)
+            )
+        means_over_time.append(patient.mean(axis=0))
+    means_over_time = np.array(means_over_time)
+    feature_means, feature_err = compute_means_errors(means_over_time)
+
+    if decay:
+        means_over_time_lin_dec = np.array(means_over_time_lin_dec)
+        feature_means_lin_dec, feature_err_lin_dec = compute_means_errors(
+            means_over_time_lin_dec
+        )
+
+        means_over_time_exp_dec = np.array(means_over_time_exp_dec)
+        feature_means_exp_dec, feature_err_exp_dec = compute_means_errors(
+            means_over_time_exp_dec
+        )
+
+    if debug:
+        # Top k feature importances
+        k = 5
+        ind = np.argpartition(feature_means, -k)[-k:].astype(int)
+        print("No decay")
+        print(np.array(xtick_labels)[ind])
+        if decay:
+            ind = np.argpartition(feature_means_lin_dec, -k)[-k:].astype(int)
+            print("Linear decay")
+            print(ind, np.array(xtick_labels)[ind])
+            ind = np.argpartition(feature_means_exp_dec, -k)[-k:].astype(int)
+            print("Exponential decay")
+            print(ind, np.array(xtick_labels)[ind])
+
+    xs = np.arange(len(feature_means))
+    plt.errorbar(
+        xs,
+        feature_means,
+        yerr=feature_err.T,
+        fmt="^",
+        label="No decay" if decay else "Mean attribution",
+    )
+    if decay:
+        plt.errorbar(
+            xs,
+            feature_means_lin_dec,
+            yerr=feature_err_lin_dec.T,
+            fmt="^",
+            label="Linear decay",
+        )
+        plt.errorbar(
+            xs,
+            feature_means_exp_dec,
+            yerr=feature_err_exp_dec.T,
+            fmt="^",
+            label="Exponential decay",
+        )
+    plt.ylabel("Attribution")
+    plt.xticks(ticks=xs, labels=xtick_labels, rotation=50, fontsize=8, ha="right")
+    if show_legend:
+        plt.legend()
+    plt.title(title)
+    plt.show()
